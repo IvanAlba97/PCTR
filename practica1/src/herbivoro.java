48545d61b71a3533eb216967323d696022c28c5b
IvanAlba97 <ivan02071997@gmail.com>
2021-10-14 11:01:16 +0200

    Terminado herbivoro

--- practica1/src/herbivoro.java
+++ practica1/src/herbivoro.java
@@ -1,10 +1,15 @@
 package PCTR.practica1.src;
 
-public class herbivoro {
+public class herbivoro extends animal{
 
-    private enum tipo { FRUGÍVORO, FOLÍVORO, XILÓFAGO, GRANÍVORO, RIZÓFAGO };
+    private enum tipo { FRUGÍVORO, FOLÍVORO, XILÓFAGO, GRANÍVORO, RIZÓFAGO }
+    tipo tipoHerbivoro;
 
-    public herbivoro(enum tipo) {
-        this.tipo = t;
+    public herbivoro(int nO, int nP, boolean pelo, tipo t) {
+        super(nO, nP, pelo);
+        this.tipoHerbivoro = t;
     }
+
+    public void setTipoHerbivoro(tipo t)    { this.tipoHerbivoro = t;   }
+    public tipo getTipoHerbivoro()          { return tipoHerbivoro;     }
 }
