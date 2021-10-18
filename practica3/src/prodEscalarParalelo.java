af6aaedaa9a248089e90b636f144c82ffae1aa6a
IvanAlba97 <ivan02071997@gmail.com>
2021-10-18 13:39:38 +0200

    Terminado producto escalar paralelo

---
+++ practica3/src/prodEscalarParalelo.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class prodEscalarParalelo extends Thread {
+    
+    private static final int N = 1000000;
+    private int idHebra, inicio, fin;
+    private static int[] v1 = new int[N], v2 = new int[N];
+    private static int prod = 0;
+    
+    
+    public prodEscalarParalelo(int idHebra, int inicio, int fin, int[] v1, int[] v2) {
+        this.idHebra = idHebra;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+    
+    @Override
+    public void run() {
+        for(int i = inicio; i < fin; i++) {
+            prod += v1[i] * v2[i];
+        }
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        Random rand = new Random(System.nanoTime());
+        Scanner S = new Scanner(System.in);
+        
+        int inicio, fin, rango;
+        
+        for(int i = 0; i < N; i++) {
+            v1[i] = rand.nextInt(10);
+            v2[i] = rand.nextInt(10);
+        }
+        
+        System.out.print("Introduce el numero de hebras: ");
+        int nHebras = S.nextInt();
+        rango = (int) (N / nHebras);
+        inicio = 0;
+        fin = rango;
+        double tiempoInicio = System.nanoTime();
+        prodEscalarParalelo[] h = new prodEscalarParalelo[nHebras];
+        for(int i = 0; i < nHebras; i++) {
+            h[i] = new prodEscalarParalelo(i, inicio, fin, v1, v2);
+            h[i].start();
+            inicio = fin;
+            if(fin + rango >= N) fin = N - 1;
+            else fin += rango;
+        }
+        for(int i = 0; i < nHebras; i++) {
+            h[i].join();
+        }
+        double tiempoFin = System.nanoTime() - tiempoInicio;
+        System.out.println("Tiempo paralelo: " + tiempoFin/1000000 + " milisegundos.");
+    }
+    
+}
