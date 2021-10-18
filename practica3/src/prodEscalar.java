ebd465033e0f278bd96611c29bf28a4ed2b2e36a
IvanAlba97 <ivan02071997@gmail.com>
2021-10-18 13:32:18 +0200

    Terminado producto escalar secuencial

---
+++ practica3/src/prodEscalar.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class prodEscalar {
+    
+    private static int N = 1000000;
+    
+    public static void main(String[] args) {
+        
+        Random rand = new Random(System.nanoTime());
+        
+        int[] v1 = new int[N];
+        int[] v2 = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            v1[i] = rand.nextInt(10);
+            v2[i] = rand.nextInt(10);
+        }
+        
+        int prod = 0;
+        
+        double tiempoInicio = System.nanoTime();
+        for(int i = 0; i < N; i++) {
+            prod += v1[i] * v2[i];
+        }
+        double tiempoFin = System.nanoTime() - tiempoInicio;
+        
+        System.out.println("Tiempo secuencial: " + tiempoFin/1000000 + " milisegundos.");
+    }
+}
