f1d407780f5687805eee966ec1578766caed67e4
IvanAlba97 <ivan02071997@gmail.com>
2021-11-15 13:59:35 +0200

    Terminado cliente multiple

--- practica6/src/clienteMultiple.java
+++ practica6/src/clienteMultiple.java
@@ -1,3 +1,42 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class clienteMultiple {
-    
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        //int i = (int) (Math.random() * 10);
+        int puerto = 2001;
+        char c;
+        do {
+            System.out.print("Introduce el número de clientes: ");
+            int nClientes = S.nextInt();
+            double tiempoInicio = System.nanoTime();
+            for (int i = 0; i < nClientes; i++) {
+                try {
+                    System.out.println("Realizando conexion...");
+                    Socket cable = new Socket("localhost", puerto);
+                    System.out.println("Realizada conexion a " + cable);
+                    PrintWriter salida = new PrintWriter(
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            cable.getOutputStream())));
+                    salida.println(i);
+                    salida.flush();
+                    System.out.println("Cerrando conexion...");
+                    cable.close();
+
+                } catch (IOException e) {
+                    System.out.println("Error en sockets...");
+                }
+            }
+            double tiempoFin = System.nanoTime() - tiempoInicio;
+            System.out.println("Tiempo: " + tiempoFin / 1000000 + " milisegundos.");
+
+            System.out.print("Desea volver a lanzar los clientes (S/N)?: ");
+            c = S.next().charAt(0);
+        } while(c == 's' || c == 'S');
+        S.close();
+    }
 }
