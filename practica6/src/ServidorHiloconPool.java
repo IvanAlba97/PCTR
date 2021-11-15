aa4e591e6f84f594b547b56e03509bee29bacba2
IvanAlba97 <ivan02071997@gmail.com>
2021-11-15 13:47:55 +0200

    Terminado el servidor con pool

---
+++ practica6/src/ServidorHiloconPool.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServidorHiloconPool implements Runnable{
+    Socket enchufe;
+    int id;
+    
+    public ServidorHiloconPool(Socket s, int i){
+        enchufe = s;
+        id = i;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(enchufe.getInputStream()));
+            String datos = entrada.readLine();
+            int j;
+            int i = Integer.parseInt(datos);
+            for(j=1; j<=20; j++){
+                System.out.println("El hilo "+ id +" esta escribiendo el dato "+i);
+            }
+            enchufe.close();
+            System.out.println("El hilo "+ id +" cierra su conexion");
+        }catch(Exception e){System.out.println("Error");}
+    }
+    
+    public static void main(String[] args){
+        int i = 0;
+        int puerto = 2001;
+        double tiempoInicio = System.nanoTime();
+        ExecutorService pool = Executors.newCachedThreadPool();
+        try{
+            ServerSocket chuff = new ServerSocket(puerto, 3000);
+            while(true){
+                System.out.println("Esperando solicitud de conexión...");
+                Socket cable = chuff.accept();
+                System.out.println("Recibida solicitud de conexión...");
+                pool.execute(new ServidorHiloconPool(cable, i));
+                i++;
+            }
+        }catch(IOException e){System.out.println("Error en los sockets...");}
+        double tiempoFin = System.nanoTime() - tiempoInicio;
+        System.out.println("TIEMPO = " + tiempoFin/1000000 + " milisegundos.");
+    }
+}
