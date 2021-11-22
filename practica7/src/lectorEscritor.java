d7657bdf9aca696b1432dc533c534582a950990e
IvanAlba97 <ivan02071997@gmail.com>
2021-11-22 13:28:28 +0200

    Terminado el ejercicio Lector-Escritor

--- practica7/src/lectorEscritor.java
+++ practica7/src/lectorEscritor.java
@@ -1,3 +1,40 @@
 public class lectorEscritor {
-    
+
+    private int n;  //Numero de lectores leyendo
+    private boolean escribiendo;
+
+    public lectorEscritor() {
+        n = 0;
+        escribiendo = false;
+    }
+
+    public synchronized void iniciaLectura(int i) throws InterruptedException {
+        while (escribiendo) {
+            wait();
+            System.out.println("Lector " + i + " esperando.");
+            n++;
+            System.out.println("Lector " + i + " leyendo.");
+        }
+    }
+
+    public synchronized void finLectura(int i) {
+        n--;
+        System.out.println("Lector " + i + " deja de leer.");
+        notifyAll();
+    }
+
+    public synchronized void iniciaEscritura(int i) throws InterruptedException {
+        while (n > 0 || escribiendo) {  //Mientras haya algun lector o alguien este escribiendo
+            wait();
+            System.out.println("Escritor " + i + " esperando.");
+        }
+        escribiendo = true;
+        System.out.println("Escritor " + i + " escribiendo.");
+    }
+
+    public synchronized void finEscritura(int i) {
+        escribiendo = false;
+        System.out.println("Escritor " + i + " deja de escribir.");
+        notifyAll();
+    }
 }
