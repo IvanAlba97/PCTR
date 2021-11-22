8e08d57c4eb7cd5a8d98180849071201c645ea0d
IvanAlba97 <ivan02071997@gmail.com>
2021-11-22 14:19:35 +0200

    Resolviendo el ejercicio de drakkarVikingo

--- practica7/src/usaDrakkarVikingo.java
+++ practica7/src/usaDrakkarVikingo.java
@@ -1,10 +1,46 @@
 public class usaDrakkarVikingo {
-    private static monitorDrakkar monitor;
+    private static drakkarVikingo monitor;
+
+    public static class vikingo implements Runnable {
+
+        private int id;
+
+        public vikingo(int hilo) {
+            this.id = hilo;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                monitor.comer(id);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static class cocinero implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                monitor.cocinar();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int nVikingos = 10;
         cocinero C = new cocinero();
-        monitor = new monitorDrakkar(nVikingos);
+        monitor = new drakkarVikingo(nVikingos);
         vikingo[] V = new vikingo[nVikingos];
         Thread[] hilos = new Thread[nVikingos];
         Thread cocinero = new Thread(C);
