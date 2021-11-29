3903a440f94ae55377765d25a514af135bdd513f
IvanAlba97 <ivan02071997@gmail.com>
2021-11-29 13:29:40 +0200

    Terminado el ejercicio 1

--- practica8/src/usaMonitorImpresion.java
+++ practica8/src/usaMonitorImpresion.java
@@ -1,3 +1,46 @@
-public class usaMonitorImpresion {
+import static java.lang.Thread.sleep;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class usaMonitorImpresion implements Runnable {
+
+    private static monitorImpresion M;
+    //private static final int nImpresoras = 3;
+    private final int id;
+
+    public usaMonitorImpresion(int id) {
+        this.id = id;
+        M = new monitorImpresion();
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                int imp = M.takePrint(id);
+                sleep(1000);
+                M.dropPrint(id, imp);
+                sleep(2000);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
     
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Introduce el numero de procesos: ");
+        int nProc = S.nextInt();
+        int nHilos = (int)(Runtime.getRuntime().availableProcessors()/1-0.5);
+        ExecutorService pool = Executors.newFixedThreadPool(nHilos);
+        for(int i=0;i<nProc;i++){
+            Runnable runnable = new usaMonitorImpresion(i);
+            pool.execute(runnable);
+        }
+        pool.shutdown();
+        while(!pool.isTerminated());
+        S.close();
+    }
 }
