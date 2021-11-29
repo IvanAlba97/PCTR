a2485eca90e095281bcd9e35ebec438a43207766
IvanAlba97 <ivan02071997@gmail.com>
2021-11-29 14:21:07 +0200

    Empezando el ejercicio 2

--- practica8/src/forkMonitor.java
+++ practica8/src/forkMonitor.java
@@ -1,3 +1,9 @@
 public class forkMonitor {
+    private static int[] fork = new int[4];
     
+    public synchronized void takeForks(int n) {
+
+    }
+
+    // TERMINARLO CON EL MONITOR TEORICO DE LAS DIAPOSITIVAS DE LA PRÁCTICA
 }
