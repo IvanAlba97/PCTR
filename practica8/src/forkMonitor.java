d45bcaf1aa1eb1477f2b37b553fc37e6a7a9df6b
IvanAlba97 <ivan02071997@gmail.com>
2021-12-06 15:58:32 +0200

    Práctica terminada

--- practica8/src/forkMonitor.java
+++ practica8/src/forkMonitor.java
@@ -1,8 +1,54 @@
 public class forkMonitor {
-    private static int[] fork = new int[4];
-    
-    public synchronized void takeForks(int n) {
 
+    private static int[] fork = new int[5];
+    private static int N;
+
+    public forkMonitor(int n) {
+        forkMonitor.N = n;
+        for(int i = 0; i < 4; i++) fork[i] = 2; 
+    }
+    
+    public synchronized void takeForks(int i) throws InterruptedException {
+        while(fork[i] != 2) wait();
+        switch(i) {
+            case 0:
+                fork[i+1] -= 1;
+                fork[N-1] -= 1;
+                break;
+            case 4:
+                fork[0] -= 1;
+                fork[i-1] -= 1;
+                break;
+            default:
+                fork[i+1] -= 1;
+                fork[i-1] -= 1;
+                break;
+        }
+        System.out.println("Filosofo " + i + " obtiene tenedores.");
+    }
+
+    public synchronized void releaseForks(int i) throws InterruptedException {
+        switch(i) {
+            case 0:
+                fork[i+1] += 1;
+                fork[N-1] += 1;
+                if(fork[i+1] == 2) notifyAll();
+                if(fork[N-1] == 2) notifyAll();
+                break;
+            case 4:
+                fork[0] += 1;
+                fork[i-1] += 1;
+                if(fork[0] == 2) notifyAll();
+                if(fork[i-1] == 2) notifyAll();
+                break;
+            default:
+                fork[i+1] += 1;
+                fork[i-1] += 1;
+                if(fork[i+1] == 2) notifyAll();
+                if(fork[i-1] == 2) notifyAll();
+                break;
+        }
+        System.out.println("Filosofo " + i + " libera tenedores.");
     }
 
     // TERMINARLO CON EL MONITOR TEORICO DE LAS DIAPOSITIVAS DE LA PRÁCTICA
--- practica8/src/usaForkMonitor.java
+++ practica8/src/usaForkMonitor.java
@@ -1,3 +1,29 @@
-public class usaForkMonitor {
-    
+public class usaForkMonitor extends Thread {
+
+    private int id;
+    private static final int N = 5;
+    private static forkMonitor monitor = new forkMonitor(N);
+
+    public usaForkMonitor(int hilo) {
+        this.id = hilo;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(5000);
+                monitor.takeForks(id);
+                Thread.sleep(1000);
+                monitor.releaseForks(id);
+            } catch (InterruptedException E) { E.printStackTrace(); }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        usaForkMonitor[] filosofos = new usaForkMonitor[N];
+        for(int i = 0; i < N; i++) {
+            filosofos[i] = new usaForkMonitor(i);
+            filosofos[i].start();
+        }
+    }
 }
