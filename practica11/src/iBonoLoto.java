cbc34bd3ec60c89d8e9e493bed4ade31d70f07b4
IvanAlba97 <ivan02071997@gmail.com>
2021-12-20 13:23:02 +0200

    Añadidas las interfaces proporcionadas por el profesor

--- practica11/src/iBonoLoto.java
+++ practica11/src/iBonoLoto.java
@@ -1,3 +1,14 @@
-public class iBonoLoto {
-    
+/**
+ * @(#)iBonoLoto.java
+ * @author A.T.
+ * @version 1.00 2013/1/2
+ */
+
+import java.rmi.*;
+public interface iBonoLoto
+  extends Remote
+{
+  public void resetServidor() throws RemoteException;
+  public boolean compApuesta(int[] apuesta)  throws RemoteException;
+
 }
