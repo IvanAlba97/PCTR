eb8f574a76a2865bfb57ba39e03e95b61aa2e7bd
IvanAlba97 <ivan02071997@gmail.com>
2021-12-20 13:44:13 +0200

    Terminado el ejercicio de BonoLoto

--- practica11/src/sBonoLoto.java
+++ practica11/src/sBonoLoto.java
@@ -1,3 +1,38 @@
-public class sBonoLoto {
-    
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
+import java.util.Random;
+
+public class sBonoLoto extends UnicastRemoteObject implements iBonoLoto {
+
+    private final int[] bonoLoto = new int[6];
+
+    public sBonoLoto() throws RemoteException {
+        resetServidor();
+    }
+
+    @Override
+    public void resetServidor() throws RemoteException {
+        Random rand = new Random(System.nanoTime());
+        for (int i = 0; i < bonoLoto.length; i++) {
+            bonoLoto[i] = rand.nextInt(2);
+        }
+    }
+
+    @Override
+    public boolean compApuesta(int[] apuesta) throws RemoteException {
+        boolean res = false;
+        System.out.println("Apuesta: " + Arrays.toString(apuesta));
+        if(Arrays.equals(bonoLoto, apuesta)) {
+            res = true;
+            System.out.println("ACIERTO");
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        iBonoLoto ORemoto = new sBonoLoto();
+        Naming.bind("//localhost/Servidor", ORemoto);
+        System.out.println("Servidor remoto preparado.");
+    }
 }
