2baf1d353a2502d3fd1bf37851b618b0e6d42daf
IvanAlba97 <ivan02071997@gmail.com>
2021-12-27 13:13:08 +0200

    Usando gnuplot y terminando la práctica

--- practica9/src/ccSem.java
+++ practica9/src/ccSem.java
@@ -1,3 +1,47 @@
+import java.util.concurrent.Semaphore;
+
 public class ccSem {
-    
+
+    private double saldo;
+    public static long iterations = 1000000;
+    public static Semaphore sem = new Semaphore(1);
+
+    public ccSem() { this.saldo = 0.0; }
+
+    public double getSaldo() { return saldo; }
+
+    public void aumentar(double cantidad) {
+        for(long i = 0; i < iterations; i++) {
+            try{
+                sem.acquire();
+            } catch(InterruptedException E) {}
+        }
+        try{
+            saldo += cantidad;
+            System.out.println("Se ha aumentado el saldo de la cuenta en " + cantidad + " unidades.");
+            System.out.println("Saldo actual: " + saldo);
+        } finally {
+            sem.release();
+        }
+    }
+
+    public void decrementar(double cantidad) {
+        for(long i = 0; i < iterations; i++) {
+            try{
+                sem.acquire();
+            } catch(InterruptedException E) {}
+        }
+        try {
+            if (saldo >= cantidad) {
+                saldo -= cantidad;
+                System.out.println("Se ha decrementado el saldo de la cuenta en " + cantidad + " unidades.");
+                System.out.println("Saldo actual: " + saldo);
+            } else {
+                System.out.println("No tiene saldo suficiente.");
+                System.out.println("Saldo disponible: " + saldo);
+            }
+        } finally {
+            sem.release();
+        }
+    }
 }
--- practica9/src/tiempos.java
+++ practica9/src/tiempos.java
@@ -1,3 +1,52 @@
-public class tiempos {
-    
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class tiempos  {
+
+	public tiempos() {}
+	
+	public static void main(String[] args) {
+
+		long iteraciones = 100000000;
+		
+		// SYNCHRONIZED
+        long n1 = 0;
+		Object lock = new Object();
+		double tiempoInicio = System.nanoTime();
+        for(int i = 0; i < iteraciones; i++) synchronized(lock) { n1++; }
+		double tiempoFin = System.nanoTime() - tiempoInicio;
+		System.out.println("SYNCHRONIZED: " + tiempoFin / 1000000 + " milisegundos");
+		
+		//SEMAPHORE
+		long n2 = 0;
+		Semaphore sem = new Semaphore(1);
+		tiempoInicio = System.nanoTime();
+        for(int i = 0; i < iteraciones; i++) {
+            try{ sem.acquire(); } catch (InterruptedException E) { E.printStackTrace(); }
+            n2++;
+            sem.release(); 
+        }
+		tiempoFin = System.nanoTime() - tiempoInicio;
+		System.out.println("SEMAPHORE: " + tiempoFin / 1000000 + " milisegundos");
+		
+		// REENTRANTLOCK
+		long n3 = 0;
+		ReentrantLock cerrojo = new ReentrantLock();
+		tiempoInicio = System.nanoTime();
+        for(int i = 0; i < iteraciones; i++) {
+            cerrojo.lock();
+            n3++;
+            cerrojo.unlock();
+        }
+		tiempoFin = System.nanoTime() - tiempoInicio;
+		System.out.println("REENTRANTLOCK: " + tiempoFin / 1000000 + " milisegundos");
+		
+		// ATOMIC
+		AtomicInteger n4 = new AtomicInteger(0);
+		tiempoInicio = System.nanoTime();
+        for(int i = 0; i < iteraciones; i++) n4.incrementAndGet();
+		tiempoFin = System.nanoTime() - tiempoInicio;
+		System.out.println("ATOMIC: " + tiempoFin / 1000000 + " milisegundos");
+	}
 }
