76775661cd868c2890e2c3c328f53fdfd5c40468
IvanAlba97 <ivan02071997@gmail.com>
2022-11-10 13:40:59 +0100

    Terminado el algoritmo de Peterson

---
+++ practica4/src/algPeterson.java
@@ -0,0 +1,58 @@
+package practica4.src;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+/**
+ * Esta clase contiene los atributos y metodos del algoritmo de Peterson.
+ * @author Iván Alba Gómez
+ * @version 3.0
+ */
+public class algPeterson implements Runnable {
+    private static boolean wantp = false, wantq = false;
+    private static int last = 1;
+    static volatile int enteroCompartido = 0;
+    static final int iteraciones = 10000;
+
+    public algPeterson() {}
+
+    @Override
+    public void run() {
+        if(Thread.currentThread().getName().equals("pool-1-thread-1")) P();
+        if(Thread.currentThread().getName().equals("pool-1-thread-2")) Q();
+    }
+
+    public void P() {
+        for(int i = 0; i < iteraciones; i++) {
+            wantp = true;
+            last = 1;
+            if(wantq = false || last == 2) Thread.yield();
+            wantp = false;
+            enteroCompartido++;
+            System.out.println(enteroCompartido);
+        }
+    }
+
+    public void Q() {
+        for(int i = 0; i < iteraciones; i++) {
+            wantq = true;
+            last = 2;
+            if(wantp = false || last == 1) Thread.yield();
+            wantq = false;
+            enteroCompartido--;
+            System.out.println(enteroCompartido);
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        for(int i = 0; i < 2; i++) pool.execute(new algPeterson());
+        pool.shutdown();
+        while(!pool.isTerminated()) {}
+        System.out.println("El valor del recurso compartido es " + enteroCompartido);
+        System.out.println("Deberia ser 0");
+    }
+}
+
