3854ee82f1926508bb4fb0a38de312bf74c184d1
IvanAlba97 <ivan02071997@gmail.com>
2022-11-10 11:35:24 +0100

    Corregido algoritmo de Dekker

--- practica4/src/algDekker.java
+++ practica4/src/algDekker.java
@@ -7,11 +7,10 @@ package practica4.src;
  */
 public class algDekker extends Thread {
 
-    static final int iteraciones = 1000000;
+    static final int iteraciones = 10000;
     static volatile int enteroCompartido = 0;
     static volatile boolean wantP = false;
     static volatile boolean wantQ = false;
-    static volatile boolean wantR = false;
     static volatile int turn = 1;
 
     /**
@@ -20,14 +19,11 @@ public class algDekker extends Thread {
     public algDekker() {
         Thread p = new P();
         Thread q = new Q();
-        Thread r = new R();
         p.start();
         q.start();
-        r.start();
         try {
             p.join();
             q.join();
-            r.join();
         } catch(InterruptedException e){
             System.err.println(e.getMessage());
         }
@@ -47,7 +43,7 @@ public class algDekker extends Thread {
         public void run() {
             for (int i = 0; i < iteraciones; i++) {
                 wantP = true;
-                while (wantQ || wantR) {
+                while (wantQ) {
                     if (turn != 1) {
                         wantP = false;
                         while (turn != 1) {
@@ -57,6 +53,7 @@ public class algDekker extends Thread {
                     }
                 }
                 enteroCompartido++;
+                System.out.println(enteroCompartido); 
                 turn = 2;
                 wantP = false;
             }
@@ -75,7 +72,7 @@ public class algDekker extends Thread {
         public void run() {
             for (int i = 0; i < iteraciones; i++) {
                 wantQ = true;
-                while (wantP || wantR) {
+                while (wantP) {
                     if (turn != 2) {
                         wantQ = false;
                         while (turn != 2) {
@@ -84,42 +81,14 @@ public class algDekker extends Thread {
                         wantQ = true;
                     }
                 }
-                enteroCompartido++;
-                turn = 3;
+                enteroCompartido--;
+                System.out.println(enteroCompartido); 
+                turn = 1;
                 wantQ = false;
             }
         }
     }
 
-    /**
-     * Esta clase representa un proceso dentro del algoritmo de Dekker.
-     */
-    class R extends Thread {    //Turno 3
-
-        /**
-         * Método que encapsula el código a ejecutar concurrentemente.
-         */
-        @Override
-        public void run() {
-            for (int i = 0; i < iteraciones; i++) {
-                wantR = true;
-                while (wantP || wantQ) {
-                    if (turn != 3) {
-                        wantR = false;
-                        while (turn != 3) {
-                            Thread.yield();
-                        }
-                        wantR = true;
-                    }
-                }
-                enteroCompartido++;
-                turn = 1;
-                wantR = false;
-            }
-        }
-    }
-
-    
     /** 
      * Método principal.
      * @param args
