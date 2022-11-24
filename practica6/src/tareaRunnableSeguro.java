79f6eeb830c72a958c80694f89476efdfbe5e789
IvanAlba97 <ivan02071997@gmail.com>
2022-11-24 15:50:49 +0100

    Eliminados ejercicios del año anterior.

--- practica6/src/tareaRunnableSeguro.java
+++
@@ -1,9 +0,0 @@
-public class tareaRunnableSeguro {
-    
-    private int n = 0;
-    
-    public tareaRunnableSeguro() {}
-    public synchronized void inc() { n++; }
-    public synchronized void dec() { n--; }
-    public int getN() { return n; }
-}
