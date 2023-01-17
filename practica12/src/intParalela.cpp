c8256cc0c07e1937d4cc10ca3a8d181b4ff4014c
IvanAlba97 <ivan02071997@gmail.com>
2023-01-17 11:22:53 +0100

    Modificato montecarlo en c++

--- practica12/src/intParalela.cpp
+++ practica12/src/intParalela.cpp
@@ -2,71 +2,46 @@
 #include <random>
 #include <thread>
 #include <mutex>
+
 using namespace std;
 
-// función a integrar
-double func(double x) {
-    return x*x;
-}
-
-// Variables compartidas
 int totalHits = 0;
-mutex m;
-
-// Rango de integración
-double xMin = 0;
-double xMax = 1;
-double yMin = 0;
-double yMax = 1;
-
-// Número de puntos a lanzar
-int nPoints;
-
-// Número de hilos
+Lock m;
+int nPuntos;
 int nThreads;
 
 void monteCarlo(int id) {
-    mt19937 gen(random_device{}());
-    uniform_real_distribution<double> xDist(xMin, xMax);
-    uniform_real_distribution<double> yDist(yMin, yMax);
-
     int hits = 0;
-    for (int i = 0; i < nPoints/nThreads; i++) {
-        double x = xDist(gen);
-        double y = yDist(gen);
-        if (y <= func(x)) {
+    for (int i = 0; i < nPuntos/nThreads; i++) {
+        double x = (static_cast<double>(rand()) / RAND_MAX);
+        double y = (static_cast<double>(rand()) / RAND_MAX);
+        if (y <= x*x) {
             hits++;
         }
     }
 
-    // Seccion critica
     m.lock();
     totalHits += hits;
     m.unlock();
 }
 
 int main() {
-    // Leer el número de puntos a lanzar y el número de tareas paralelas
     cout << "Introduce el número de puntos a lanzar: ";
-    cin >> nPoints;
+    cin >> nPuntos;
     cout << "Introduce el número de tareas paralelas: ";
     cin >> nThreads;
 
-    // Crear hilos
     thread threads[nThreads];
     for (int i = 0; i < nThreads; i++) {
         threads[i] = thread(monteCarlo, i);
     }
 
-    // Unir hilos
     for (int i = 0; i < nThreads; i++) {
         threads[i].join();
     }
 
-    // Calcular el área bajo la curva
-    double area = (xMax - xMin) * (yMax - yMin) * (double) totalHits / nPoints;
+    double area = (double) totalHits / nPuntos;
 
-    // Imprimir el resultado
     cout << "El área bajo la curva es: " << area << endl;
 
     return 0;
